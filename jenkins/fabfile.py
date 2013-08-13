c529c71b86a42db461d361db99a6500c5f7146e4
Melissa Avery-Weir <averymd@irrsinn.net>
2013-08-13 13:59:33 -0400

    Server builds from fabric

---
+++ jenkins/fabfile.py
@@ -0,0 +1,18 @@
+from __future__ import with_statement
+from fabric.api import settings, abort, run, cd, env
+
+env.hosts = ['192.241.189.7']
+env.user = 'root'
+
+def deploy_clean_jenkins():
+  run('wget http://mirrors.jenkins-ci.org/war/latest/jenkins.war')
+  run('cp jenkins.war /var/lib/tomcat7/webapps')
+
+def deploy_scripts():
+	return
+
+def setup_server():
+  run('apt-get update')
+  run('apt-get install -y tomcat7 default-jdk')
+  run('mkdir /var')
+  run('/etc/init.d/tomcat7 start')
