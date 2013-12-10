b5deea18d9bcec29bc419f44b3da846efb6dbed1
Melissa Avery-Weir <averymd@irrsinn.net>
2013-12-10 11:30:19 -0500

    Fix if/else logic for pattern choice
    In addition, overwrite existing file.

--- blink1/new-mail.ps1
+++ blink1/new-mail.ps1
@@ -1,6 +1,9 @@
 $outlook = New-Object -com Outlook.Application
 $inbox = $outlook.Session.GetDefaultFolder(6)
-$stream = [System.IO.StreamWriter] "d:\blink-new-mail-temp.txt"
-if ($inbox.UnReadItemCount -eq 0) { $stream.WriteLine("#000000") }
-$stream.WriteLine("pattern: ""NewMail""")
+$stream = New-Object 'System.IO.StreamWriter' -ArgumentList "d:\blink-new-mail-temp.txt", $false
+if ($inbox.UnReadItemCount -eq 0) { 
+    $stream.WriteLine("#000000") 
+} else {
+    $stream.WriteLine("pattern: ""NewMail""")
+}
 $stream.close()
